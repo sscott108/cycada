82a9d688676bcbec3b92334218f6ab3282c364b5
Sarah Scott <skarah00@gmail.com>
2024-03-20 11:35:35 -0400

    update

---
+++ current_phases/basic_utils.py
@@ -0,0 +1,376 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+import torch
+from PIL import Image
+import os
+import matplotlib.pyplot as plt
+from torch.utils.data import Dataset
+import numpy as np
+import random
+import torch
+import torch.nn as nn
+import config
+import copy
+import torchvision.transforms as transforms
+from torchvision.utils import make_grid
+from torch.utils.data import DataLoader
+from torchvision import datasets
+from torch.autograd import Variable
+import torch.nn as nn
+import torch.nn.functional as F
+import torch
+import rasterio
+from rasterio.windows import Window
+import numpy as np
+from os import listdir
+from os.path import isfile, join
+np.random.seed(42)
+import copy
+import matplotlib
+import matplotlib.pyplot as plt
+import pickle as pkl
+import cv2
+from PIL import Image, ImageEnhance
+import itertools
+import torchvision.transforms as transforms
+import glob
+from tqdm.notebook import tqdm
+import warnings
+from torchvision.utils import make_grid
+import functools
+from sklearn.model_selection import train_test_split
+import pandas as pd
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+print(f'On {device}')
+Tensor  = torch.cuda.FloatTensor
+import torchvision.models as models
+
+# data (img)
+img_height = 256
+img_width = 256
+channels = 3
+
+# training
+epoch = 0 # epoch to start training from
+n_epochs = 50 # number of epochs of training
+batch_size = 1 # size of the batches
+lr = 0.0002 # adam : learning rate
+b1 = 0.5 # adam : decay of first order momentum of gradient
+b2 = 0.999 # adam : decay of first order momentum of gradient
+decay_epoch = 3 # suggested default : 100 (suggested 'n_epochs' is 200)
+                 # epoch from which to start lr decay
+
+    
+transforms_ = [
+    transforms.Resize(int(img_height*1.12), Image.BICUBIC),
+    transforms.RandomCrop((img_height, img_width)),
+    transforms.RandomHorizontalFlip(),
+    transforms.ToTensor(),
+    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+]
+class Multi_City_CNN(nn.Module):
+    def __init__(self):
+        super(Multi_City_CNN, self).__init__()
+        
+        net = models.resnet18(pretrained=True)
+        
+        for param in net.parameters():
+            param.requires_grad = True #True=finetune
+        
+        self.resnet_pretrained = net
+        self.fc1 = nn.Linear(self.resnet_pretrained.fc.out_features, 512)
+        self.dropout = nn.Dropout(p=0.2)
+        self.relu = nn.ReLU()
+        self.fc2 = nn.Linear(512, 512)
+        self.fc3 = nn.Linear(512, 1)
+        
+    def forward(self, image):
+        img_features = self.resnet_pretrained(image)
+        img_features = torch.flatten(img_features, 1)
+        img_features = self.fc1(img_features.float())
+        x = self.relu(img_features)
+        x = self.dropout(x)
+        x = self.fc2(x.float())
+        x = self.relu(x)
+        x = self.dropout(x)
+        x = self.fc3(x)
+        return x
+# Where to save the figures
+PROJECT_ROOT_DIR = "."
+PROJECT_SAVE_DIR = "Figure_PDFs"
+
+import os
+if not (os.path.isdir(PROJECT_ROOT_DIR+'/'+PROJECT_SAVE_DIR)):
+    print('Figure directory didn''t exist, creating now.')
+    os.mkdir(PROJECT_ROOT_DIR+'/'+PROJECT_SAVE_DIR)
+else:
+    print('Figure directory exists.')   
+
+def to_rgb(image):
+    rgb_image = Image.new("RGB", image.size)
+    rgb_image.paste(image)
+    return rgb_image
+
+def weights_init_normal(m):
+    classname = m.__class__.__name__
+    if classname.find('Conv') != -1:
+        torch.nn.init.normal_(m.weight.data, 0.0, 0.02) # reset Conv2d's weight(tensor) with Gaussian Distribution
+        if hasattr(m, 'bias') and m.bias is not None:
+            torch.nn.init.constant_(m.bias.data, 0.0) # reset Conv2d's bias(tensor) with Constant(0)
+        elif classname.find('BatchNorm2d') != -1:
+            torch.nn.init.normal_(m.weight.data, 1.0, 0.02) # reset BatchNorm2d's weight(tensor) with Gaussian Distribution
+            torch.nn.init.constant_(m.bias.data, 0.0) # reset BatchNorm2d's bias(tensor) with Constant(0)
+            
+class CyDataset(Dataset):
+    def __init__(self):
+
+        self.D = []
+        self.L = []
+                
+        with open('/datacommons/carlsonlab/srs108/old/ol/Delhi_clean.pkl', "rb") as fp:
+            for station in tqdm(pkl.load(fp)):
+                self.D.append(tuple((station['Image'][:,:,:3], station['PM25'])))
+                
+        with open('/datacommons/carlsonlab/srs108/old/ol/Beijing_clean.pkl', "rb") as fp:
+            for station in tqdm(pkl.load(fp)):
+                self.L.append(tuple((station['Image'][:,:,:3], station['PM25'])))
+#                 for datapoint in station:
+#                     luck_img = datapoint['Image'][:,:,:3]
+#                     if luck_img.shape == (224, 224,3):  
+#                         self.L.append(tuple((luck_img, datapoint['PM'])))
+                        
+        self.D = random.choices(self.D, k= len(self.L))
+        
+    def __len__(self): return (len(self.D))
+        
+    def __getitem__(self, idx):
+        if torch.is_tensor(idx):
+            idx = idx.tolist()
+
+            #Delhi normalization
+        transform  = transforms.Compose([
+                            transforms.ToPILImage(),
+                            transforms.RandomHorizontalFlip(),
+                            transforms.RandomVerticalFlip(),
+                            transforms.Pad(16),
+                            transforms.ToTensor(),
+#                             transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))
+        ])
+
+        d_img = self.D[idx][0]
+        d_img = transform(d_img)        
+        l_img = self.L[idx][0]
+        l_img = transform(l_img)
+        
+        sample = {
+              'A': d_img,
+#               'D pm' : torch.tensor(self.D[idx][1]),
+              'B': l_img,
+#               'L pm' : torch.tensor(self.L[idx][1])
+        }
+        return sample
+    
+class ResidualBlock(nn.Module):
+    def __init__(self, in_features):
+        super(ResidualBlock, self).__init__()
+        
+        self.block = nn.Sequential(
+            nn.ReflectionPad2d(1), # Pads the input tensor using the reflection of the input boundary
+            nn.Conv2d(in_features, in_features, 3),
+            nn.InstanceNorm2d(in_features), 
+            nn.ReLU(inplace=True),
+            nn.ReflectionPad2d(1),
+            nn.Conv2d(in_features, in_features, 3),
+            nn.InstanceNorm2d(in_features)
+        )
+
+    def forward(self, x):
+        return x + self.block(x)
+
+
+class GeneratorResNet(nn.Module):
+    def __init__(self, input_shape, num_residual_block):
+        super(GeneratorResNet, self).__init__()
+        
+        channels = input_shape[0]
+        
+        # Initial Convolution Block
+        out_features = 64
+        model = [
+            nn.ReflectionPad2d(channels),
+            nn.Conv2d(channels, out_features, 7),
+            nn.InstanceNorm2d(out_features),
+            nn.ReLU(inplace=True)
+        ]
+        in_features = out_features
+        
+        # Downsampling
+        for _ in range(2):
+            out_features *= 2
+            model += [
+                nn.Conv2d(in_features, out_features, 3, stride=2, padding=1),
+                nn.InstanceNorm2d(out_features),
+                nn.ReLU(inplace=True)
+            ]
+            in_features = out_features
+        
+        # Residual blocks
+        for _ in range(num_residual_block):
+            model += [ResidualBlock(out_features)]
+            
+        # Upsampling
+        for _ in range(2):
+            out_features //= 2
+            model += [
+                nn.Upsample(scale_factor=2), # --> width*2, heigh*2
+                nn.Conv2d(in_features, out_features, 3, stride=1, padding=1),
+                nn.ReLU(inplace=True)
+            ]
+            in_features = out_features
+            
+        # Output Layer
+        model += [nn.ReflectionPad2d(channels),
+                  nn.Conv2d(out_features, channels, 7),
+                  nn.Tanh()
+                 ]
+        
+        # Unpacking
+        self.model = nn.Sequential(*model) 
+        
+    def forward(self, x):
+        return self.model(x)
+    
+    
+class Discriminator(nn.Module):
+    def __init__(self, input_shape):
+        super(Discriminator, self).__init__()
+        
+        channels, height, width = input_shape
+        
+        # Calculate output shape of image discriminator (PatchGAN)
+        self.output_shape = (1, height//2**4, width//2**4)
+        
+        def discriminator_block(in_filters, out_filters, normalize=True):
+            """Returns downsampling layers of each discriminator block"""
+            layers = [nn.Conv2d(in_filters, out_filters, 4, stride=2, padding=1)]
+            if normalize:
+                layers.append(nn.InstanceNorm2d(out_filters))
+            layers.append(nn.LeakyReLU(0.2, inplace=True))
+            return layers
+        
+        self.model = nn.Sequential(
+            *discriminator_block(channels, 64, normalize=False),
+            *discriminator_block(64, 128),
+            *discriminator_block(128,256),
+            *discriminator_block(256,512),
+            nn.ZeroPad2d((1,0,1,0)),
+            nn.Conv2d(512, 1, 4, padding=1)
+        )
+        
+    def forward(self, img):
+        return self.model(img)
+    
+    
+class GANLoss(nn.Module):
+
+    def __init__(self, target_real_label=1.0, target_fake_label=0.0):
+        """ Initialize the GANLoss class.
+        Parameters:
+            target_real_label (bool) - - label for a real image
+            target_fake_label (bool) - - label of a fake image
+        Note: Do not use sigmoid as the last layer of Discriminator.
+        LSGAN needs no sigmoid. vanilla GANs will handle it with BCEWithLogitsLoss.
+        """
+        super(GANLoss, self).__init__()
+        self.register_buffer('real_label', torch.tensor(target_real_label))
+        self.register_buffer('fake_label', torch.tensor(target_fake_label))
+        self.loss = nn.BCEWithLogitsLoss()
+
+    def get_target_tensor(self, prediction, target_is_real):
+        """Create label tensors with the same size as the input.
+        Parameters:
+            prediction (tensor) - - tpyically the prediction from a discriminator
+            target_is_real (bool) - - if the ground truth label is for real images or fake images
+        Returns:
+            A label tensor filled with ground truth label, and with the size of the input
+        """
+
+        if target_is_real:
+            target_tensor = self.real_label
+        else:
+            target_tensor = self.fake_label
+        return target_tensor.expand_as(prediction)
+
+    def __call__(self, prediction, target_is_real):
+        """Calculate loss given Discriminator's output and grount truth labels.
+        Parameters:
+            prediction (tensor) - - tpyically the prediction output from a discriminator
+            target_is_real (bool) - - if the ground truth label is for real images or fake images
+        Returns:
+            the calculated loss.
+        """
+        target_tensor = self.get_target_tensor(prediction, target_is_real)
+        loss = self.loss(prediction, target_tensor)
+        return loss
+    
+class LambdaLR:
+    def __init__(self, n_epochs, offset, decay_start_epoch):
+        assert (n_epochs - decay_start_epoch) > 0, "Decay must start before the training session ends!"
+        self.n_epochs = n_epochs
+        self.offset = offset
+        self.decay_start_epoch = decay_start_epoch
+        
+    def step(self, epoch):
+        return 1.0 - max(0, epoch+self.offset - self.decay_start_epoch)/(self.n_epochs - self.decay_start_epoch)
+    
+class Flatten(nn.Module):
+    def forward(self, input):
+        return input.view(input.size(0), -1)
+
+class FeatureDiscriminator(nn.Module):
+    def __init__(self):
+        super(FeatureDiscriminator, self).__init__()
+        self.discriminator = nn.Sequential(
+            nn.Linear(1, 275),
+            nn.ReLU(),
+            nn.Linear(275, 500),
+            nn.ReLU(),
+            nn.Linear(500, 1),
+            nn.Sigmoid()
+        )
+
+    def forward(self, score):
+        out = self.discriminator(score)
+        return out
+    
+class LeNet(nn.Module): #for 32
+    def __init__(self, input_nc):
+        super(LeNet, self).__init__()
+
+        sequence = [
+            nn.Conv2d(input_nc, 20, kernel_size=5, stride=1, padding=0),
+            nn.MaxPool2d(2, 2),
+            nn.ReLU(True),
+
+            nn.Conv2d(20, 50, kernel_size=5, stride=1, padding=0),
+            nn.Dropout2d(0.5),
+            nn.MaxPool2d(2, 2),
+            nn.ReLU(True),
+
+            Flatten(),
+
+            nn.Linear(50*5*5, 500),
+            nn.ReLU(True),
+            nn.Dropout(0.5),
+
+            nn.Linear(500, 1)
+        ]
+        self.net = nn.Sequential(*sequence)
+
+    def forward(self, input, d_feat=False):
+        """Standard forward."""
+        self.out = self.net(input)
+        return self.out
